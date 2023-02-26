ee8c1f1028801f1d7ccd8c3886e0989c1e052e37
alexkwabena352@gmail.com <alexkwabena352@gmail.com>
2023-02-26 12:46:42 +0000

    New

---
+++ Readme.md
@@ -0,0 +1,2 @@
+###### Welcome to this cool 😎 challenge 
+by frontend mentor to also practice more challenges like this visit [frontendmentor.io](https://www.frontendmentor.io)
---
+++ advice-generator-app-main/advice.js
@@ -0,0 +1,11 @@
+const generator = document.querySelector('.generator')
+generator.addEventListener('click', ()=>{
+ fetch('https://api.adviceslip.com/advice')
+.then(response => response.json())
+.then(function(data){
+ const {id, advice} = data.slip
+ document.querySelector('.advice-id').innerHTML = `Advice #${id}`
+ document.querySelector('.advice-text').innerHTML = `"${advice}"`
+}) 
+.catch(err => {})
+})
