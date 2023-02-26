57987b87e4ea84283016b3ffacdcb467922ea502
alexkwabena352@gmail.com <alexkwabena352@gmail.com>
2023-02-26 16:59:29 +0000

    New

--- advice-generator-app-main/advice.js
+++ advice-generator-app-main/advice.js
@@ -4,8 +4,10 @@ generator.addEventListener('click', ()=>{
 .then(response => response.json())
 .then(function(data){
  const {id, advice} = data.slip
+ 
  document.querySelector('.advice-id').innerHTML = `Advice #${id}`
- document.querySelector('.advice-text').innerHTML = `"${advice}"`
+ document.querySelector('.advice-text').innerHTML = `" ${advice} "`
 }) 
 .catch(err => {})
+ 
 })
